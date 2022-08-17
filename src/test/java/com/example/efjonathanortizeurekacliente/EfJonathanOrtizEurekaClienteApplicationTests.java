075ad39155f37af6a50d8ab5c2328b055820a6e4
JhonasPaul <91173212+JhonasPaul@users.noreply.github.com>
2022-08-16 22:06:18 -0500

    evaluacion final

---
+++ src/test/java/com/example/efjonathanortizeurekacliente/EfJonathanOrtizEurekaClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.efjonathanortizeurekacliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfJonathanOrtizEurekaClienteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
